2bdd25e88e0bcf255ba38d87f3663c3e20b74b9a
shubham-xumit <shubxam@gmail.com>
2023-01-30 11:51:40 +0530

    update

--- dashboard-generation.py
+++ dashboard-generation.py
@@ -1,120 +1,25 @@
 # Imports
 import pandas as pd
 # import numpy as np
-# from urllib.request import urlopen, Request
-# from bs4 import BeautifulSoup
-# from pprint import pprint
-# from nsepython import *
 from datetime import datetime
-# import time
-
-# import nltk
-# nltk.downloader.download('vader_lexicon')
-# from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import plotly
 import plotly.express as px
 
 import streamlit as st
 
-# # Get company tickers and create a dataframe
-# ticker_url = 'https://www1.nseindia.com/content/indices/ind_nifty500list.csv'
-# tickers_file = pd.read_csv(ticker_url)
-# tickers_df = tickers_file[['Symbol', 'Company Name']]
-# tickers = tickers_df['Symbol']
 
-# # Get articles and put them into a df
-# news_url = 'https://ticker.finology.in/company/'
-
-# # list to store article data
-# data = []
-# unavailable_tickers = []
-# companies_len = len(tickers)
-# length = 100
-
-# for i in range(length):
-#     #print(i)
-#     req = Request(url= '{}/{}'.format(news_url, tickers[i]),headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0'})
-#     response = urlopen(req)
-#     html = BeautifulSoup(response, 'lxml') 
-#     news_links = html.select('#newsarticles > a')     #select('.newslink')  
-#     if len(news_links) == 0:
-#         print('No news found for {}'.format(tickers[i]))
-#         unavailable_tickers.append(tickers[i])
-#         # delete all ticker from tickers and tickers_df together #todo
-#         continue
-
-#     for link in news_links:
-#         title = link.find('span', class_='h6').text
-#         #separate date and time from datetime object
-#         date_time_obj = datetime.strptime(link.find('small').text, '%d %b %Y, %I:%M%p')
-#         art_date = date_time_obj.date().strftime('%Y/%m/%d')
-#         art_time = date_time_obj.time().strftime('%H:%M')
-#         data.append([tickers[i], title, art_date, art_time])  
-#     ###if (i != 0 and i%200 == 0):
-#     ####print('sleeping')
-#     ####time.sleep(30)
-# df = pd.DataFrame(data, columns=['Ticker', 'Headline', 'Date', 'Time'])
-
-# # removing unavailable tickers from original df
-# for ticker in unavailable_tickers:
-#     if ticker in tickers:
-#         tickers.drop(ticker, inplace=True)
-
-# tickers_df.drop(tickers_df[tickers_df['Symbol'].isin(unavailable_tickers)].index.values, inplace=True)
-
-# # Sentiment Analysis
-# vader = SentimentIntensityAnalyzer()
-# scores = df['Headline'].apply(vader.polarity_scores).tolist()
-# scores_df = pd.DataFrame(scores)
-# new_df = pd.merge(left=df, right=scores_df, on=df.index.values).drop(['key_0'], axis=1)
-# final_df = new_df.groupby('Ticker').mean()
-
-# # Get Company,Sector,Industry,mCap Data
-# sector = []
-# industry = []
-# mCap = []
-# for i in range(length):
-#     meta = nse_eq(tickers[i])
-#     #print(tickers[i])
-#     try:
-#         sector.append(meta['industryInfo']['macro'])
-#     except KeyError:
-#         print('{} is not available'.format(tickers[i]))
-#         sector.append(np.nan)
-#         industry.append(np.nan)
-#         mCap.append(np.nan)
-#         continue
-#     #pprint('Sector: {}'.format(meta['industryInfo']['macro']))
-#     try:
-#         industry.append(meta['industryInfo']['sector'])
-#     except KeyError:
-#         print('{} is not available'.format(tickers[i]))
-#         industry.append(np.nan)
-#         mCap.append(np.nan)
-#         continue
-#     #pprint('Industry: {}'.format(meta['industryInfo']['sector']))
-#     try:
-#         mCap.append(round((meta['priceInfo']['previousClose'] * meta['securityInfo']['issuedSize'])/1000000000, 2))
-#     except KeyError:
-#         print('{} is not available'.format(tickers[i]))
-#         mCap.append(np.nan)
-#     #print('market cap is Rs {}'.format(ticker_mcap))
-#     #print('\n')
-
-# final_df['sector'] = sector
-# final_df['industry'] = industry
-# final_df['mCap (Billion)'] = mCap
-
-# final_df = final_df.reset_index()
-# final_df = pd.merge(final_df, tickers_df, left_on='Ticker', right_on='Symbol').drop('Symbol', axis=1)
-# final_df.columns = ['Symbol', 'Negative', 'Neutral', 'Positive', 'Sentiment Score', 'Sector', 'Industry', 'MCap (Billion)', 'Company Name']
-
-# final_df = final_df.dropna()
-
+# read csv files
+#XC indices list for reference
 xc_indices = pd.read_csv('xc-indices.csv', header=0)
+
+article_data = pd.read_csv('./datasets/NIFTY_500_Articles.csv')
+
+
+## Filter Articles by date
+
+## Filter articles by UNIVERSE
 
 sentiment_data = pd.read_csv('sentiment_data.csv')
 
