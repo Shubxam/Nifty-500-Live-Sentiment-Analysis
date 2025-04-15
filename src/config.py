659b3c34a0526fbab0c2dcbe3bde34bcefd00ad3
Shubham Soni <shubxam@gmail.com>
2025-04-15 20:34:57 +0530

    fix: update sentiment model name to 'yiyanghkust/finbert-tone' and set batch size to 8

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
-SENTIMENT_MODEL_NAME = "mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis"
+SENTIMENT_MODEL_NAME = 'yiyanghkust/finbert-tone'
+BATCH_SIZE = 8
 HEADER: dict[str, str] = {
             "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:20.0) Gecko/20100101 Firefox/20.0",
             "Accept": "application/json"
