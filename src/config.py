3d6e8f45b1f2b90d44f8cf6bd5a2021857d968b2
Shubham Soni <shubxam@gmail.com>
2025-04-19 13:26:18 +0530

    add config.py

---
+++ src/config.py
@@ -0,0 +1,20 @@
+# convert this Literals into enums
+
+from typing import Literal
+
+IndexType = Literal["nifty_50", "nifty_100", "nifty_200", "nifty_500"]
+
+UNIVERSE_NAMES_DICT: dict[str, str] = {
+    "nifty_50": "NIFTY 50",
+    "nifty_100": "NIFTY 100",
+    "nifty_200": "NIFTY 200",
+    "nifty_500": "NIFTY 500",
+}
+
+DatePickerOptions = Literal[
+    "Past 24 Hours",
+    "Past 7 days",
+    "Past 1 Month",
+    "Past 2 Months",
+    "Full",
+]
